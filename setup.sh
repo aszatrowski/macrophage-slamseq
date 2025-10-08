d6cd383cf8604e99c9093050b55754114c4524ac
Austin Szatrowski <austinszatrowski@gmail.com>
2025-10-08 09:41:49 -0500

    setup file actually works now—specify env at command line, validates env path, and everything else auto-executes and persists in shell (with source setup.sh myenv)

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
+#!/bin/bash
 export USE_CONDA_CACHE=0
-echo "$USE_CONDA_CACHE"
+echo "conda cache:" "$USE_CONDA_CACHE"
 module load python/miniforge-25.3.0
-source activate /project/lbarreiro/USERS/austin/envs/slamseq_v1
-source activate /project/lbarreiro/USERS/austin/envs/slamseq_fastp
+echo "Loaded python/miniforge-25.3.0"
 
-snakemake --executor slurm --default-resources --jobs 1
+ENV_NAME="${1:-myenv}"
+
+if [ -d "/project/lbarreiro/USERS/austin/envs/$ENV_NAME" ]; then
+    source activate /project/lbarreiro/USERS/austin/envs/"$ENV_NAME"
+    echo "Activated conda env $ENV_NAME."
+    echo "Setup complete."
+else
+    echo "Error: Environment $ENV_NAME not found at /project/lbarreiro/USERS/austin/envs/$ENV_NAME"
+    return 1 
+fi
+
+# snakemake --executor slurm --default-resources --jobs 1
 # conda create -c bioconda --prefix=/project/lbarreiro/USERS/austin/envs/snakemake python=3.11 snakemake snakemake-executor-plugin-slurm
 # conda create -c bioconda -c conda-forge --prefix=/project/lbarreiro/USERS/austin/envs/slamseq_v1 python=3.11 snakemake snakemake-executor-plugin-slurm fastqc trimmomatic star hisat-3n kallisto samtools bedtools openjdk
-conda create -c bioconda -c conda-forge --prefix=/project/lbarreiro/USERS/austin/envs/slamseq_fastp python=3.11 snakemake snakemake-executor-plugin-slurm fastqc fastp trimmomatic star hisat-3n kallisto samtools bedtools openjdk
+# conda create -c bioconda -c conda-forge --prefix=/project/lbarreiro/USERS/austin/envs/slamseq_fastp python=3.11 snakemake snakemake-executor-plugin-slurm fastqc fastp trimmomatic star hisat-3n kallisto samtools bedtools openjdk
 
 # source activate, not conda activate! per https://docs.rcc.uchicago.edu/software/apps-and-envs/python/
 # source activate /project/lbarreiro/USERS/austin/envs/snakemake
