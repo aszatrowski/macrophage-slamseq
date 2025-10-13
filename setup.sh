8a1170d00c6eeed498be87adf83a6a01a8067e61
Austin Szatrowski <austinszatrowski@gmail.com>
2025-10-13 11:33:40 -0500

    add multiqc to conda, update README and env.yaml

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ fi
 # conda create -c bioconda --prefix=/project/lbarreiro/USERS/austin/envs/snakemake python=3.11 snakemake snakemake-executor-plugin-slurm
 # conda create -c bioconda -c conda-forge --prefix=/project/lbarreiro/USERS/austin/envs/slamseq_v1 python=3.11 snakemake snakemake-executor-plugin-slurm fastqc trimmomatic star hisat-3n kallisto samtools bedtools openjdk
 # conda create -c bioconda -c conda-forge --prefix=/project/lbarreiro/USERS/austin/envs/slamseq_fastp python=3.11 snakemake snakemake-executor-plugin-slurm fastqc fastp trimmomatic star hisat-3n kallisto samtools bedtools openjdk
-# conda create -c conda-forge -c bioconda --prefix=/project/lbarreiro/USERS/austin/envs/slamseq_v2 snakemake snakemake-executor-plugin-slurm snakemake-storage-plugin-http fastp fastqc hisat-3n samtools bedtools kallisto=0.50.1 openjdk  
+# conda create -c conda-forge -c bioconda --prefix=/project/lbarreiro/USERS/austin/envs/slamseq_v2 snakemake snakemake-executor-plugin-slurm snakemake-storage-plugin-http fastp fastqc hisat-3n samtools bedtools kallisto=0.50.1 openjdk multiqc
 
 # source activate, not conda activate! per https://docs.rcc.uchicago.edu/software/apps-and-envs/python/
 # source activate /project/lbarreiro/USERS/austin/envs/snakemake
