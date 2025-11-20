dcfb0300fb92d5d69dc918214f1a647ec0cc30bd
Austin Szatrowski <austinszatrowski@gmail.com>
2025-11-20 11:17:46 -0600

    complete apptainer launch command

--- apptainer_build.sh
+++ apptainer_build.sh
@@ -1,2 +1,2 @@
 apptainer build containers/gedi_1.0.6a.sif slamseq/gedi.def
-apptainer shell --bind /project/lbarreiro/USERS/austin,/scratch/midway3/aszatrowski
+apptainer shell --bind /project/lbarreiro/USERS/austin,/scratch/midway3/aszatrowski /project/lbarreiro/USERS/austin/containers/gedi_1.0.6a.sif
