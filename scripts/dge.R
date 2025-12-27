9db7e98d7ff3ee9cdac6bd27746f1f342733b15d
Austin Szatrowski <austinszatrowski@gmail.com>
2025-12-26 22:03:57 -0600

    generalize DGE to handle all timepoint comparisons. two persistent errors: something pre-processing related in `total` and all the nascent 15m FDRs are stuck at 1

--- scripts/dge.R
+++ scripts/dge.R
@@ -1,33 +1,63 @@
 library(edgeR)
 nascent_counts_df <- readr::read_csv(snakemake@input$merged_counts, show_col_types = FALSE) |>
-  dplyr::select("Gene", starts_with("t_0m"), starts_with("t_120m")) |>
   dplyr::mutate(across(where(is.double), ~ replace(.x, is.na(.x), 0)))
 
+timepoints <- as.factor(gsub("t_(.+)m_nascent.*", "\\1", colnames(nascent_counts_df)[-1]))
+
 dge <- DGEList(
-	counts = nascent_counts_df[, -1],
-  group = c(rep("t_0m", 2), rep("t_120m", 2)),
-	genes = nascent_counts_df$Gene
+  counts = nascent_counts_df[, -1],
+  group = timepoints,
+  genes = nascent_counts_df$Gene
 )
+
 
 # Filter lowly expressed genes
 keep <- filterByExpr(dge)
 dge <- dge[keep, , keep.lib.sizes=FALSE]
+
+# Calculate normalization factors for library size and composition bias
 dge <- calcNormFactors(dge)
 
-design <- model.matrix(~ group, data = dge$samples)
+# Design matrix for timepoints: compares each timepoint to baseline (t_0m), with intercepts, no interactions
+design <- model.matrix(~ timepoints, data = dge$samples)
 
 fit <- glmQLFit(dge, design)
 
+symbol_ensg_mapping <- readr::read_csv(snakemake@input$symbol_ensg_mapping, show_col_types = FALSE)
 # Test for differential expression (t_120m vs t_0m)
-qlf <- glmQLFTest(fit, coef = 2)
-# coef=2 tests the t_120m coefficient (the time effect)
+qlf_list <- lapply(
+  seq(2, length(colnames(design))), # all coefficients except intercept
+  function(coef) {
+    glmQLFTest(fit, coef = coef)
+  }
+)
+# remove "timepoints" prefix from names
+names(qlf_list) <- stringr::str_replace(colnames(design)[-1], "timepoints", "")
+# order list by timepoint numeric value
+qlf_list <- qlf_list[order(as.numeric(names(qlf_list)))]
 
 # Extract results
-results <- topTags(qlf, n = Inf, adjust.method = "BH", sort.by = "PValue")
-
-symbol_ensg_mapping <- readr::read_csv(snakemake@input$symbol_ensg_mapping, show_col_types = FALSE)
-summstats <- results$table |>
-  dplyr::left_join(symbol_ensg_mapping, by = c("genes" = "Gene")) |>
-  dplyr::rename(Gene = "Symbol", ENSG = "genes") |>
-  dplyr::select(Gene, ENSG, logFC, logCPM, F, PValue, FDR)
-readr::write_csv(summstats, snakemake@output$dge_summary_stats)
+export_results <- function(qlf, timepoint, mapping) {
+  results <- topTags(qlf, n = Inf, adjust.method = "BH", sort.by = "PValue")$table
+  print(head(results))
+  results <- results |>
+    dplyr::left_join(symbol_ensg_mapping, by = c("genes" = "Gene")) |>
+    dplyr::rename(Gene = "Symbol", ENSG = "genes") |>
+    dplyr::mutate(
+      timepoint = timepoint,
+      comparison = paste(timepoint, "vs", "0m", sep = "_")) |>
+    dplyr::select(Gene, timepoint, comparison, logFC, logCPM, F, PValue, FDR, ENSG)
+  return(results)
+}
+dge_results <- do.call(
+  rbind,
+  mapply(
+    export_results,
+    qlf = qlf_list,
+    timepoint = names(qlf_list),
+    MoreArgs = list(mapping = symbol_ensg_mapping),
+    SIMPLIFY = FALSE
+  )
+)
+head(dge_results)
+readr::write_csv(dge_results, snakemake@output$dge_summary_stats)
